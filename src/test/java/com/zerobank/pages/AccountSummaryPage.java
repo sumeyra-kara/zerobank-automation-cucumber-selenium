53013586a0bee322b3f431478ce6c3c6840bb549
karasumeyra <sumeyrakara93@gmail.com>
2024-06-13 17:33:16 +0200

    test

--- src/test/java/com/zerobank/pages/AccountSummaryPage.java
+++ src/test/java/com/zerobank/pages/AccountSummaryPage.java
@@ -10,7 +10,6 @@ public class AccountSummaryPage extends BasePage{
     @FindBy(xpath = "//div[@class='offset2 span8']//h2") // accountSummary hesap turleri
     public List<WebElement> accountTypes;
 
-
     @FindBy(xpath = "(//table[@class='table'])[3]/thead//th")  //creditAccount sütunlari
     public List<WebElement> creditAccountColumns;
 
